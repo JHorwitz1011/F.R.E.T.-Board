3c364e2bb4de31ba700c8ed7d51728963af2d63d
Jim Horwitz <jhorwitz1011@gmail.com>
2023-11-18 16:08:24 -0500

    i2c adc pushed

--- code/release/ads7830.c
+++ code/release/ads7830.c
@@ -8,12 +8,12 @@ ads7830_write(i2c_inst_t* i2c, uint8_t ch, uint8_t pwr_mode) {
     i2c_write_blocking(i2c, ADS7830_ADDR_WRITE, msg, 1, false);
 }
 
-void ads7830_read(i2c_inst_t* i2c, uint8_t *dst, uint8_t ch) {
+void ads7830_read(i2c_inst_t* i2c, uint8_t *dst, uint8_t ch, uint8_t pwr_mode) {
     uint8_t dst = 0;
     uint8_t msg = (ADS7830_CMD_SE << ADS7830_PORT_BITS_OFFSET) | (ch << ADS7830_CH_BITS_OFFSET) | (pwr_mode << ADS7830_PD_BITS_OFFSET);
     i2c_write_blocking(i2c, ADS7830_ADDR_WRITE, msg, 1, false);
     i2c_read_blocking(i2c, ADS7830_ADDR_READ, &dst, 1, false)
-    return dst;
+    return dst;\
 }
 
 void ads7830_init(i2c_inst_t* i2c) {
@@ -22,5 +22,5 @@ void ads7830_init(i2c_inst_t* i2c) {
     gpio_set_function(SCL, GPIO_FUNC_I2C);
 
     // set default configuration / channel to 0
-    ads8930_write(i2c, ADS7830_CH0, )
+    ads8930_write(i2c, ADS7830_CH0, ADS7830_PD_IREF_OFF_AD_BT_CONV_ON);
 }
