79a7056d972a1c67ad293e4f3303ba0f5dfea752
Jim Horwitz <jhorwitz1011@gmail.com>
2023-11-24 12:05:42 -0500

    fixed point testing

--- code/examples/filtering/filters.h
+++ code/examples/filtering/filters.h
@@ -1,47 +1,47 @@
 #pragma once
 
-typedef signed int sfint32_t;
+typedef signed int sfint;
 
-#define mulsfint32_t(a,b) ((sfint32_t)(((( signed long long )(a))*(( signed long long )(b)))>>16)) //multiply two fixed 16:16
-#define float_to_sfint32_t(a) ((sfint32_t)((a)*65536.0)) // 2^16
-#define sfint32_t_to_float(a) ((float)(a)/65536.0)
-#define sfint32_t_to_int(a)    ((int)((a)>>16))
-#define int_to_sfint32_t(a)    ((sfint32_t)((a)<<16))
-#define divsfint32_t(a,b) ((sfint32_t)((((signed long long)(a)<<16)/(b)))) 
-#define abssfint32_t(a) abs(a)
+#define mulsfint(a,b) ((sfint)(((( signed long long )(a))*(( signed long long )(b)))>>16)) //multiply two fixed 16:16
+#define float_to_sfint(a) ((sfint)((a)*65536.0)) // 2^16
+#define sfint_to_float(a) ((float)(a)/65536.0)
+#define sfint_to_int(a)    ((int)((a)>>16))
+#define int_to_sfint(a)    ((sfint)((a)<<16))
+#define divsfint(a,b) ((sfint)((((signed long long)(a)<<16)/(b)))) 
+#define abssfint(a) abs(a)
 
 
 typedef struct Coeff{
     //high pass diff equation coefficients
-    sfint32_t l_a[3];
-    sfint32_t l_b[3];
+    sfint l_a[3];
+    sfint l_b[3];
     //lower mid diff equation coefficients
-    sfint32_t m1_a[3];
-    sfint32_t m1_b[3];
+    sfint m1_a[3];
+    sfint m1_b[3];
     //middle mid diff equation coefficients
-    sfint32_t m2_a[3];
-    sfint32_t m2_b[3];
+    sfint m2_a[3];
+    sfint m2_b[3];
     //high mid diff equation coefficients
-    sfint32_t m3_a[3];
-    sfint32_t m3_b[3];
+    sfint m3_a[3];
+    sfint m3_b[3];
     //high pass diff equation coefficients
-    sfint32_t h_a[3];
-    sfint32_t h_b[3];
+    sfint h_a[3];
+    sfint h_b[3];
 } Coeff;
 
 typedef struct Gains {
-    sfint32_t l;
-    sfint32_t m1;
-    sfint32_t m2;
-    sfint32_t m3;
-    sfint32_t h;
+    sfint l;
+    sfint m1;
+    sfint m2;
+    sfint m3;
+    sfint h;
 } Gains;
 
-void lows(sfint32_t* x, sfint32_t* y, Coeff* coeffs);
-void mid1s(sfint32_t* x, sfint32_t* y, Coeff* coeffs);
-void mid2s(sfint32_t* x, sfint32_t* y, Coeff* coeffs);
-void mid3s(sfint32_t* x, sfint32_t* y, Coeff* coeffs);
-void highs(sfint32_t* x, sfint32_t* y, Coeff* coeffs);
+void lows(sfint* x, sfint* y, Coeff* coeffs);
+void mid1s(sfint* x, sfint* y, Coeff* coeffs);
+void mid2s(sfint* x, sfint* y, Coeff* coeffs);
+void mid3s(sfint* x, sfint* y, Coeff* coeffs);
+void highs(sfint* x, sfint* y, Coeff* coeffs);
 
 
 /***
@@ -58,4 +58,4 @@ void deinitGains(Gains* gains);
  * First, pop the output buffer leaving y[0] empty
  * Passes the x and y buffer y through all five filters and sums them to be constructs y[0]
  ***/
-void filter(sfint32_t* x, sfint32_t* y, Coeff* coeffs, Gains* gains);
+void filter(sfint* x, sfint* y, Coeff* coeffs, Gains* gains);
--- code/examples/filtering/main.c
+++ code/examples/filtering/main.c
@@ -5,77 +5,11 @@
 
 #define BUFFER_LEN 10000
 
-void printFixedBinary(sfint32_t input) {
-    sfint32_t copy = input;
-    for(int i = 0; i < sizeof(sfint32_t); i++) {
-        printf("%d", (input && (0x01 << i)) >> i );
-    }
-}
 
-void printIntBinary(int input) {
-    int copy = input;
-    for(int i = 0; i < sizeof(int); i++) {
-        printf("%d", (input && (0x01 << i)) >> i );
-    }
-}
+void dumpBuffer(int freq, float* wave, sfint castedWave, sfint filteredWave, int n) {
+        printf("for a wave of frequency %d\n", freq);
+        printf("the original float wave has an average of %f, max of %f, and min of %f.\n", averageFloat(wave, BUFFER_LEN), maxFloat(wave, BUFFER_LEN), minFloat(wave, BUFFER_LEN));
 
-float minFloat(float* buffer, int n) {
-    float min = 2;
-    for(int i = 0; i < n; i++) {
-        if(buffer[i] < min)
-            min = buffer[i];
-    }
-    return min;
-}
-
-sfint32_t minFixed(sfint32_t* buffer, int n) {
-    sfint32_t min = 65535;
-    for(int i = 0; i < n; i++) {
-        if(buffer[i] < min)
-            min = buffer[i];
-    }
-    return min;
-}
-
-float maxFloat(float* buffer, int n) {
-    float max = -2;
-    for(int i = 0; i < n; i++) {
-        if(buffer[i] > max)
-            max = buffer[i];
-    }
-    return max;
-}
-
-sfint32_t maxFixed(sfint32_t* buffer, int n) {
-    sfint32_t max = 0;
-    for(int i = 0; i < n; i++) {
-        if(buffer[i] > max)
-            max = buffer[i];
-    }
-    return max;
-}
-
-
-void averageFloat(float* buffer, int n) {
-    float sum = 0;
-    for(int i = 0; i< n; i++) {
-        sum += buffer[i]/((float)n)
-    }
-}
-
-void averageFixed(sfint32_t* buffer, int n) {
-    
-}
-
-void dumpBuffer(int freq, float* wave, sfint32_t castedWave, sfint32_t filteredWave, int n) {
-        printf("For frequency %f\n 
-                The original wave has average %f min %f and max %f\n 
-                The casted wave has average %f min %f and max %f\n 
-                The filtered wave has average %f min %f and max %f\n\n", 
-                freq, 
-                averageFloat(wave, BUFFER_LEN), minFloat(wave, BUFFER_LEN), maxFloat(wave, BUFFER_LEN),
-                averageFixed(castedWave, BUFFER_LEN), minFixed(castedWave, BUFFER_LEN), maxFixed(BUFFER_LEN),
-                averageFixed(castedWave, BUFFER_LEN), minFixed(castedWave, BUFFER_LEN), maxFixed(BUFFER_LEN));
 }
 
 void main() {
@@ -84,8 +18,8 @@ void main() {
     // //cos buffer
     // float time[BUFFER_LEN];
     // float wave[BUFFER_LEN]; 
-    // sfint32_t castedWave[BUFFER_LEN];
-    // sfint32_t filteredWave[BUFFER_LEN];
+    // sfint castedWave[BUFFER_LEN];
+    // sfint filteredWave[BUFFER_LEN];
 
     // for(int i = 0; i < BUFFER_LEN; i++) {
     //     time[i] = ((float)i)*.0001;
@@ -94,6 +28,7 @@ void main() {
     // for(int freq = 1; freq < 10000; freq+= 100) { //hz
         
     // }
+
     
     
 
