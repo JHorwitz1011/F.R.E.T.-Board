e4f5a49740a57563d5a7121a731b90b14591617b
Jim Horwitz <jhorwitz1011@gmail.com>
2023-12-03 15:06:23 -0500

    refactored filters

--- code/release/filters.h
+++ code/release/filters.h
@@ -8,7 +8,6 @@ typedef signed int sfint;
 // #define divsfint(a,b) ((sfint)((((signed long long)(a)<<15)/(b)))) 
 // #define abssfint(a) abs(a)
 
-
 #define float_to_sfint(a) (sfint)(a * 65536.0) // 2^15
 #define sfint_to_float(a) (((float)(a)) / 65536.0)
 
@@ -21,12 +20,40 @@ typedef signed int sfint;
 #define sfint_to_uint16(a) ((uint16_t)((a+MIDPOINT) >> 16))
 #define uint16_to_sfint(a) ((sfint)((a<<16) + MIDPOINT))
 
-// uint16_t sfint_to_uint16_fn(sfint a) {
-//     a = ((a >> 15) + MIDPOINT);
-//     a += MIDPOINT;
-//     a =
-//     return (uint16_t)(aa >> 15);
-// }
+#define HIGHB0 1.0
+#define HIGHB1 -2.0
+#define HIGHB2 1.0
+#define HIGHA0 1.00
+#define HIGHA1 -1.394137940850646
+#define HIGHA2 0.5390287685792179
+
+#define MID3B0 0.0504
+#define MID3B1 0
+#define MID3B2 -0.0504
+#define MID3A0 1.00
+#define MID3A1 -1.8527
+#define MID3A2 0.8992
+
+#define MID2B0 0.0205
+#define MID2B1 0
+#define MID2B2 -0.0205
+#define MID2A0 1.00
+#define MID2A1 -1.9516
+#define MID2A2 0.9591
+
+#define MID1B0 0.0082
+#define MID1B1 0
+#define MID1B2 -0.0082
+#define MID1A0 1.00
+#define MID1A1 -1.9824
+#define MID1A2 0.9836
+
+#define LOWB0 0.0001
+#define LOWB1 0.0002
+#define LOWB2 0.0001
+#define LOWA0 1.00
+#define LOWA1 -1.9752
+#define LOWA2 0.9755
 
 typedef struct Coeff{
     //high pass diff equation coefficients
@@ -46,6 +73,12 @@ typedef struct Coeff{
     sfint h_b[3];
 } Coeff;
 
+#define DEFAULT_LOW_GAIN 1.0f
+#define DEFAULT_MID1_GAIN 1.0f
+#define DEFAULT_MID2_GAIN 1.0f
+#define DEFAULT_MID3_GAIN 1.0f
+#define DEFAULT_HIGH_GAIN 1.0f
+
 typedef struct Gains {
     sfint l;
     sfint m1;
@@ -54,12 +87,17 @@ typedef struct Gains {
     sfint h;
 } Gains;
 
-void lows(sfint* x, sfint* y, Coeff* coeffs);
-void mid1s(sfint* x, sfint* y, Coeff* coeffs);
-void mid2s(sfint* x, sfint* y, Coeff* coeffs);
-void mid3s(sfint* x, sfint* y, Coeff* coeffs);
-void highs(sfint* x, sfint* y, Coeff* coeffs);
+sfint lows(sfint* x, sfint* y, Coeff* coeffs);
+sfint mid1s(sfint* x, sfint* y, Coeff* coeffs);
+sfint mid2s(sfint* x, sfint* y, Coeff* coeffs);
+sfint mid3s(sfint* x, sfint* y, Coeff* coeffs);
+sfint highs(sfint* x, sfint* y, Coeff* coeffs);
 
+float lowFloat(float* x, float* y);
+float mid1Float(float* x, float* y);
+float mid2Float(float* x, float* y);
+float mid3Float(float* x, float* y);
+float highFloat(float* x, float* y);
 
 /***
  * 
@@ -75,4 +113,6 @@ void deinitGains(Gains* gains);
  * First, pop the output buffer leaving y[0] empty
  * Passes the x and y buffer y through all five filters and sums them to be constructs y[0]
  ***/
-void filter(sfint* x, sfint* y, Coeff* coeffs, Gains* gains);
+sfint filter(sfint* x, sfint* y, Coeff* coeffs, Gains* gains);
+float filterFloat(float* x, float* y);
+
